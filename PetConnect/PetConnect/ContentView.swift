444594800fff18f40b42af20fda453537963326c
wongbingg <wb277@naver.com>
2024-08-12 12:32:24 +0900

    add: 프로젝트 생성

---
+++ PetConnect/PetConnect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetConnect
+//
+//  Created by 이원빈 on 8/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetConnect/PetConnect/PetConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetConnectApp.swift
+//  PetConnect
+//
+//  Created by 이원빈 on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PetConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
