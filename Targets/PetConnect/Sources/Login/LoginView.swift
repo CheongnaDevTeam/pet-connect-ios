bce709cc467c9a1c932d4d86073d4253f8572296
wongbingg <wb277@naver.com>
2024-09-03 16:44:57 +0900

    refactor: 로그인 뷰 수정

--- Targets/PetConnect/Sources/Login/LoginView.swift
+++ Targets/PetConnect/Sources/Login/LoginView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import DesignSystem
 
 import ComposableArchitecture
+import AuthenticationServices
 
 struct LoginView: View {
   let store: StoreOf<LoginFeature>
@@ -17,15 +18,35 @@ struct LoginView: View {
   var body: some View {
     VStack {
       Spacer()
-      Button {
-        store.send(.kakaoLoginButtonTapped)
-      } label: {
-        Image(asset: DesignSystemAsset.kakaoLoginButton)
+      kakaoLoginButton
+      appleLoginButton
+    }
+  }
+  
+  var kakaoLoginButton: some View {
+    Button {
+      store.send(.kakaoLoginButtonTapped)
+    } label: {
+      ZStack {
+        RoundedRectangle(cornerRadius: 10)
+          .foregroundColor(.yellow)
+          .frame(width: 300, height: 45)
+        Text("카카오 로그인")
+          .foregroundColor(.black)
       }
-      Button {
-        store.send(.appleLoginButtonTapped)
-      } label: {
+    }
+  }
+  
+  var appleLoginButton: some View {
+    Button {
+      store.send(.appleLoginButtonTapped)
+    } label: {
+      ZStack {
+        RoundedRectangle(cornerRadius: 10)
+          .foregroundColor(.black)
+          .frame(width: 300, height: 45)
         Text("애플 로그인")
+          .foregroundColor(.white)
       }
     }
   }
